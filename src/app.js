01d3945677ca964fa3955a2b7f8ec5b3e74dbd31
=Chensokheng <=chensokheng18@kit.edu.kh>
2019-04-17 09:49:08 +0700

    make the partials for the navigation and the footer

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const hbs = require("hbs");
+const path = require("path");
+
 const app = express();
 
 // port for every host
@@ -7,15 +10,15 @@ const port = process.env.PORT || 3000;
 
 
 //connect to the public folder
-const path = require("path");
 const publicDir = path.join(__dirname, "../public");
-const viewDir = path.join(__dirname, '../views');
-app.use(express.static(publicDir));
+const templateView = path.join(__dirname, '../templates/views');
+const partialDir = path.join(__dirname, '../templates/partials');
 
 // connect with the hbs
-const hbs = require("hbs");
+app.use(express.static(publicDir));
 app.set('view engine', 'hbs');
-app.set("views", viewDir);
+app.set("views", templateView);
+hbs.registerPartials(partialDir);
 
 
 
